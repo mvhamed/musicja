47b4156ef49cbd952a881107292b87deeb0ee493
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-14 17:32:51 +0200

    Update stop.py

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -10,7 +10,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    command(["/stop", "اسكت", "انهاء", "ايقاف"]) & ~BANNED_USERS
+    filters.command(["/stop", "اسكت", "انهاء", "ايقاف"]) & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
