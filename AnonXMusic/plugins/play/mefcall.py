a11e3834ea48f1451a30aaaed568ed02196eaeea
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-14 21:21:35 +0200

    Update mefcall.py

--- AnonXMusic/plugins/play/mefcall.py
+++ AnonXMusic/plugins/play/mefcall.py
@@ -3,47 +3,47 @@ from AnonXMusic import app
 import asyncio
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from AnonXMusic.core.call import Anony
+from AnonXMusic.core.call import Alexa
 from AnonXMusic.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
 @app.on_message(filters.regex("مين في الكول"))
 async def strcall(client, message):
-    assistant = await group_assistant(Anony,message.chat.id)
+    assistant = await group_assistant(Alexa,message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./AnonXMusic/assets/call.mp3"), stream_type=StreamType().pulse_stream)
-        text="الناس الموجودين في المكالمة:\n\n"
+        await assistant.join_group_call(message.chat.id, AudioPiped("./assets/call.mp3"), stream_type=StreamType().pulse_stream)
+        text="الناس الكاعده تسولف :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتحدث 🕷️"
+                mut="يتكلم"
             else:
-                mut="ساكت 🕷️"
+                mut="ساكت"
             user = await client.get_users(participant.user_id)
             k +=1
-            text +=f"{k}- {user.mention} {mut}\n"
-        text += f"\nعددهم: {len(participants)}\n✔️"    
+            text +=f"{k}➤{user.mention}➤{mut}\n"
+        text += f"\nعددهم : {len(participants)}\n✔️"    
         await message.reply(f"{text}")
         await asyncio.sleep(7)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"المكالمة مغلقة مسبقاً\n")
+        await message.reply(f"المكالمه مغلقه الان")
     except TelegramServerError:
-        await message.reply(f"يرجى إعادة إرسال الأمر، هناك مشكلة في خادم التليجرام\n")
+        await message.reply(f"قم بارسال الامر مره اخرى")
     except AlreadyJoinedError:
-        text="الناس الموجودين في المكالمة:\n\n"
+        text="الناس الي بتتكلم :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتحدث 🕷️"
+                mut="يتكلم"
             else:
-                mut="ساكت 🕷️"
+                mut="ساكت"
             user = await client.get_users(participant.user_id)
             k +=1
-            text +=f"{k}- {user.mention} {mut}\n"
-        text += f"\nعددهم : {len(participants)}\n✔️"    
+            text +=f"{k}➤{user.mention}➤{mut}\n"
+        text += f"\العدد : {len(participants)}\n✔️"    
         await message.reply(f"{text}")
