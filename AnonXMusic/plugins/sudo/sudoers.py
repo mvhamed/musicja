15dc8079eb4450e4aeb8ab6a71e5aafbe9cd8f79
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-14 15:57:59 +0200

    Update sudoers.py

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -10,7 +10,7 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
-@app.on_message(command(["/addsudo", "رفع مطور", "/رفع مطور"]) & filters.user(OWNER_ID))
+@app.on_message(filters.command(["/addsudo", "رفع مطور", "/رفع مطور"]) & filters.user(OWNER_ID))
 @language
 async def useradd(client, message: Message, _):
     if not message.reply_to_message:
@@ -27,7 +27,7 @@ async def useradd(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-@app.on_message(command(["/delsudo", "تنزيل مطور", "/تنزيل مطور"]) & filters.user(OWNER_ID))
+@app.on_message(filters.command(["/delsudo", "تنزيل مطور", "/تنزيل مطور"]) & filters.user(OWNER_ID))
 @language
 async def userdel(client, message: Message, _):
     if not message.reply_to_message:
@@ -44,7 +44,7 @@ async def userdel(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-@app.on_message(command(["قائمة المطورين", "المطورين", "/المطورين"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["قائمة المطورين", "المطورين", "/المطورين"]) & ~BANNED_USERS)
 @language
 async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
