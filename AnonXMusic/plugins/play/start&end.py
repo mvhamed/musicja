9e14bcdec2c5c0e662c7154e29fb0c8aa7b0fd4d
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-14 20:09:36 +0200

    Update start&end.py

--- AnonXMusic/plugins/play/start&end.py
+++ AnonXMusic/plugins/play/start&end.py
@@ -1,11 +1,6 @@
-import asyncio
 from pyrogram import Client, filters
-from strings import get_command
-from strings.filters import command
-from pyrogram.types import Message, VoiceChatStarted, VoiceChatEnded
-from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import get_assistant
+from pyrogram.types import Message
+from AnonXMusic import app
 
 @app.on_message(filters.voice_chat_started)
 async def stcall(client: Client, message: Message): 
