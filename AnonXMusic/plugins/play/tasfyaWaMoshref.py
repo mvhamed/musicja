e4386b1987b1afa52b89c8592c4aaefab1249e67
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-14 20:04:19 +0200

    Update tasfyaWaMoshref.py

--- AnonXMusic/plugins/play/tasfyaWaMoshref.py
+++ AnonXMusic/plugins/play/tasfyaWaMoshref.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import ChatPermissions #ChatPrivileges
 import asyncio, requests 
-from AnonX import app
-#by > @PROGRAMMER_TOM / @BENN_DEV
+from AnonXMusic import app
+
 
 welcome_enabled = True
 
