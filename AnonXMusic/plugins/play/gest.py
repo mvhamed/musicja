32bc2d8e210780f9eab75f378073b743449e515f
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-01-21 16:58:54 +0200

    Update gest.py

--- AnonXMusic/plugins/play/gest.py
+++ AnonXMusic/plugins/play/gest.py
@@ -6,42 +6,20 @@ from pyrogram import filters, Client
 from AnonXMusic import app
 from config import *
 
-bot_name = {}
-botname = {}
-
-name = "جورجينا"
-
-@app.on_message(filters.regex("تعيين اسم البوت")& filters.private & SUDOERS, group=7113)
-async def set_bot(client: Client, message):
-   NAME = await client.Anony(message.chat.id,"**♪ ارسل اسم البوت الجديد  💎 .**", filters=filters.text, timeout=30)
-   BOT_NAME = NAME.text
-   bot_username = client.me.username
-   await set_bot_name(bot_username, BOT_NAME)
-   await message.reply_text("**♪ تم تعين اسم البوت بنجاح  💎 .**")
-
-caesar_responses = [
-    "اسمي {name} يصحبي",
-    "يسطا قولتلك اسمي {name} الاه",
-    "نعم يحب",
-    "قول يقلبو",
-    "يسطا هوا عشان بحبك تصعدني؟",
-    "يعم والله بحبك بس ناديلي ب {name}",
-    "تعرف بالله هحبك أكتر لو ناديتلي {name}",
-    "اي ي معلم مين مزعلك",
-    "متصلي على النبي كدا",
-    "مش فاضيلك نصايح وكلمني",
-    "يسطا قولي مين مزعلك وعايزك تقعد وتتفرج على أخوك",
-    "انجز عايزني أشقطلك مين؟",
-    "شكلها منكدا عليك وجاي تطلعهم علينا",
-    "ورحمة أبويا اسمي {name}",
-]
-
-@app.on_message(filters.command(["بوت", "البوت"], ""), group=71135)
-async def bottttt(client: Client, message: Message):
-    bot_username = client.me.username
-    BOT_NAME = await get_bot_name(bot_username)
-    bar = random.choice(selections).format(BOT_NAME)
-    await message.reply_text(f"**[{bar}](https://t.me/{bot_username}?startgroup=True)**", disable_web_page_preview=True)
-    
-#مقدم من القيصر صاحب العظمه @c_a_s_e_r_h                
-#قناه سورس القيصر صاحب العظمه  @COURSE_CAESAR
+@app.on_message(~filters.private)
+async def booot(client: Client, message: Message):
+    chat_id = message.chat.id
+    if not await is_served_chat(client, chat_id):
+       try:
+        await add_served_chat(client, chat_id)
+        chats = len(await get_served_chats(client))
+        bot_username = client.me.username
+        dev = await get_dev(bot_username)
+        username = f"https://t.me/{message.chat.username}" if message.chat.username else None
+        mention = message.from_user.mention if message.from_user else message.chat.title
+        await client.send_message(dev, f"**تم تفعيل محادثة جديدة تلقائياً واصبحت {chats} محادثة**\nNew Group : [{message.chat.title}]({username})\nId : {message.chat.id} \nBy : {mention}", disable_web_page_preview=True)
+        await client.send_message(chat_id, f"**تم رفع البوت بنجاح ايها العضو اللطيف 🥷**")
+        return 
+       except:
+          pass
+    message.continue_propagation()
