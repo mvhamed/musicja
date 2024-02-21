63e5069d374c30aa3eb159443d96e8144b9bc952
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-02-21 17:13:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mvhamed/JaaMusic",
+    "https://github.com/mvhamed/musicja",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
