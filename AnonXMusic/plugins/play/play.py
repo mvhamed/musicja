d402905ff543e3d0909bb2de84f4b8ad83709238
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-02-21 18:01:01 +0200

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -7,7 +7,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Mody
+from AnonXMusic.core.call import Anony
 from AnonXMusic.utils import seconds_to_min, time_to_seconds
 from AnonXMusic.utils.channelplay import get_channeplayCB
 from AnonXMusic.utils.decorators.language import languageCB
