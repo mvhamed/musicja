7df720c8ede4777220ec5dc8cdb1d5cfa92490aa
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-02-26 22:58:31 +0200

    Update create&close.py

--- AnonXMusic/plugins/play/create&close.py
+++ AnonXMusic/plugins/play/create&close.py
@@ -31,7 +31,7 @@ async def get_group_call(
     await app.send_message(f"**No group call Found** {err_msg}")
     return False
 
-@app.on_message(filters.regex("^افتح المكالمه$"))
+@app.on_message(filters.regex("^افتح كول$"))
 async def start_group_call(c: Client, m: Message):
     chat_id = m.chat.id
     assistant = await get_assistant(chat_id)
@@ -90,7 +90,7 @@ async def start_group_call(c: Client, m: Message):
         await msg.edit_text("تم فتح المكالمه بنجاح ⚡️~!")
       except:
          await msg.edit_text("خلي البوت معاه صلاحية اضافة مشرفين والتحكم ف المحادثه الصوتيه او خلي الاك المساعد رول وجرب ⚡")
-@app.on_message(filters.regex("^اقفل المكالمه$"))
+@app.on_message(filters.regex("^اقفل كول$"))
 async def stop_group_call(c: Client, m: Message):
     chat_id = m.chat.id
     assistant = await get_assistant(chat_id)
