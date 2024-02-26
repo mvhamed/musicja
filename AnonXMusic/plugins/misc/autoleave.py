ca1d9c9175dd735765c9f616a39f1b5c1c112c7b
mvhamed <145144709+mvhamed@users.noreply.github.com>
2024-02-26 17:09:35 +0200

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -10,30 +10,37 @@ from AnonXMusic.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(900):
-            from AnonXMusic.core.userbot import assistants
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        while not await asyncio.sleep(
+            config.AUTO_LEAVE_ASSISTANT_TIME
+        ):
+            from ZeMusic.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
                 left = 0
                 try:
-                    async for i in client.get_dialogs():
-                        if i.chat.type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
+                    async for i in client.iter_dialogs():
+                        chat_type = i.ChatType
+                        if chat_type in [
+                            SUPERGROUP,
+                            GROUP ,
+                            CHANNEL ,
                         ]:
+                            chat_id = i.chat.id
                             if (
-                                i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                chat_id != config.LOG_GROUP_ID
+                                and chat_id != -1001686672798
+                                and chat_id != -1001840101403
+                                and chat_id != -1001549206010
                             ):
                                 if left == 20:
                                     continue
-                                if not await is_active_chat(i.chat.id):
+                                if not await is_active_chat(chat_id):
                                     try:
-                                        await client.leave_chat(i.chat.id)
+                                        await client.leave_chat(
+                                            chat_id
+                                        )
                                         left += 1
                                     except:
                                         continue
@@ -46,8 +53,7 @@ asyncio.create_task(auto_leave())
 
 async def auto_end():
     while not await asyncio.sleep(5):
-        ender = await is_autoend()
-        if not ender:
+        if not await is_autoend():
             continue
         for chat_id in autoend:
             timer = autoend.get(chat_id)
@@ -59,13 +65,13 @@ async def auto_end():
                     continue
                 autoend[chat_id] = {}
                 try:
-                    await Anony.stop_stream(chat_id)
+                    await Elhyba.stop_stream(chat_id)
                 except:
                     continue
                 try:
                     await app.send_message(
                         chat_id,
-                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+                        "**» غادر البوت المكالمه بسبب عدم تواجد ناس في المكالمه .**",
                     )
                 except:
                     continue
